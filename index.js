d8e95035b0d60ca1b9634a6821f34faedc7f1b5d
shahidaac <shaahidaac@gmail.com>
2021-04-08 19:06:21 +0600

    cors issue

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const client = new MongoClient(uri, {
 });
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // restrict calls to those this address
+    methods: "GET", // only allow GET requests
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 5000;
@@ -21,21 +26,19 @@ const port = process.env.PORT || 5000;
 client.connect((err) => {
   const stickerCollection = client.db("stickerStore").collection("stickers");
   const orderCollection = client.db("stickerStore").collection("orders");
-  
+
   app.post("/addSticker", (req, res) => {
     const sticker = req.body;
     stickerCollection.insertOne(sticker).then((result) => {
-        console.log(result);
+      console.log(result);
       res.send(result.insertedCount > 0);
     });
   });
 
   app.post("/addOrder", (req, res) => {
-      
     const order = req.body;
     orderCollection.insertOne(order).then((result) => {
       res.send(result.insertedCount > 0);
-      console.log("db connected addorder");
     });
   });
 
@@ -44,7 +47,6 @@ client.connect((err) => {
       .find({ email: req.query.email })
       .toArray((err, documents) => {
         res.send(documents);
-        console.log("db connected orders");
       });
   });
 
@@ -58,7 +60,6 @@ client.connect((err) => {
     stickerCollection
       .find({ _id: objectId(req.params.id) })
       .toArray((err, documents) => {
-          
         res.send(documents[0]);
       });
   });
@@ -67,7 +68,6 @@ client.connect((err) => {
     stickerCollection
       .find({ email: req.query.email })
       .toArray((err, documents) => {
-          
         res.send(documents);
       });
   });
@@ -76,7 +76,6 @@ client.connect((err) => {
     stickerCollection
       .findOneAndDelete({ _id: objectId(req.params.id) })
       .then((result) => {
-        console.log(result);
         res.send(result.deletedCount > 0);
       });
   });
